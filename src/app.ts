c857584330765bc3a68956c7d9ad5838c39d21e0
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-05 23:45:26 +0530

    test: Remove all imports and check

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono"
 
-import postsRouter from "@/routes/posts.route"
-import userRouter from "@/routes/users.route"
+// import postsRouter from "@/routes/posts.route"
+// import userRouter from "@/routes/users.route"
 
 const app = new Hono()
 
@@ -9,7 +9,7 @@ app.get("/", (c) =>
   c.json("Hello from Hono, running on Bun and deployed on Vercel.")
 )
 
-app.route("/api/users", userRouter)
-app.route("/api/posts", postsRouter)
+// app.route("/api/users", userRouter)
+// app.route("/api/posts", postsRouter)
 
 export default app
