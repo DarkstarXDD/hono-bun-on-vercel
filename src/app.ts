a19b5a3efa13caa6264a106d4466b3ec328efa59
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-06 17:50:56 +0530

    test: Remove path alias

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono"
 
-import postsRouter from "@/routes/posts.route"
-import userRouter from "@/routes/users.route"
+import postsRouter from "./routes/posts.route"
+import userRouter from "./routes/users.route"
 
 const app = new Hono()
 
