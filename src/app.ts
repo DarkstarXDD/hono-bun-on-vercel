14bf0864045b41a044138f3176a7b71319edf349
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-06 18:57:48 +0530

    test: Add .js extensions

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono"
 
-import postsRouter from "./routes/posts.route"
-import userRouter from "./routes/users.route"
+import postsRouter from "./routes/posts.route.js"
+import userRouter from "./routes/users.route.js"
 
 const app = new Hono()
 
