50780f2cd7096cc565608a1dac52e6a9b60d8f3c
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-12 15:30:23 +0530

    test: Bun with .ts extensions

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono"
 
-import postsRouter from "./routes/posts.route.js"
-import userRouter from "./routes/users.route.js"
+import postsRouter from "./routes/posts.route.ts"
+import userRouter from "./routes/users.route.ts"
 
 const app = new Hono()
 
